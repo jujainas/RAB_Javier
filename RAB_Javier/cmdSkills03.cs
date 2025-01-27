cb33c6183975bb20de10f79474552cba3ff831bb
Javier.PerezMarin <javierperezmarin89@gmail.com>
2025-01-27 17:45:01 +0700

    a

--- RAB_Javier/cmdSkills03.cs
+++ RAB_Javier/cmdSkills03.cs
@@ -10,13 +10,34 @@
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document doc = uidoc.Document;
 
-            // Your Module 03 Skills code goes here
-            // Delete the TaskDialog below and add your code
-            TaskDialog.Show("Module 03 Skills", "Got Here!");
+            // 2. Create instance of class
+            Building building1 = new Building("Big Office Building", "10 Main Street", 10, 150000);
+            Building building2 = new Building("Fancy Hotel", "15 Main Street", 15, 200000);
+
 
 
             return Result.Succeeded;
         }
+    }
+
+    // 1. Define a class
+    public class Building
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int NumberOfFloors { get; set; }
+        public double Area { get; set; }
+
+        //3. add constructor to class
+        public Building(string _name, string _address, int _numberOfFloors, double _area)
+
+        {
+            Name = _name;
+            Address = _address;
+            NumberOfFloors = _numberOfFloors;
+            Area = _area;
+        }
+
     }
 
 }
